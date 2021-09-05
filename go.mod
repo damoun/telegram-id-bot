17eb680c5afa91be1f6176ede4b66ab40fa4469a
Damien PLENARD <damien@plenard.me>
2021-09-05 18:18:20 +0300

    update code to support HTTP API Gateway Proxy v2

--- go.mod
+++ go.mod
@@ -1,7 +1,8 @@
 module github.com/damoun/telegram-id-bot
 
+go 1.16
+
 require (
-	github.com/aws/aws-lambda-go v1.13.1 // indirect
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/aws/aws-lambda-go v1.26.0
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.0.0-rc1
 )
--- main.go
+++ main.go
@@ -3,34 +3,57 @@ package main
 import (
 	"os"
 	"strconv"
+	"context"
+	"net/http"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
+
+var (
+	telegramBotApi *tgbotapi.BotAPI
+	telegramApiToken = os.Getenv("TELEGRAM_API_TOKEN")
+)
+
+// Initialize Telegram API client
+func init() {
+	var err error
+	telegramBotApi, err = tgbotapi.NewBotAPI(telegramApiToken)
+	if err != nil {
+		panic(err)
+	}
+}
 
 // GetReply Create the string to reply.
 func GetReply(update tgbotapi.Update) string {
-	if update.Message.Text == "/getid" {
-		return strconv.Itoa(update.Message.From.ID)
-	} else if update.Message.Text == "/getgroupid" {
-		if update.Message.Chat.Type != "private" {
-			return strconv.FormatInt(update.Message.Chat.ID, 10)
+	if update.Message.IsCommand() {
+		switch update.Message.Command() {
+			case "getid":
+				return strconv.Itoa(update.Message.From.ID)
+			case "getgroupid":
+				if update.Message.Chat.Type != "private" {
+					return strconv.FormatInt(update.Message.Chat.ID, 10)
+				}
+				return "This is not a group! Stop fooling me!"
 		}
-		return "This is not a group! Stop fooling me!"
 	}
 	return `This bot can supply reply with your Telegram user ID.
 Use /getid command to get your Telegram ID.`
 }
 
-//WebHookHandler Handler for Telegram Webhook.
-func WebHookHandler(update tgbotapi.Update) error {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
-	if err == nil {
+// WebHookHandler Handler for Telegram Webhook.
+func WebHookHandler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	var update tgbotapi.Update
+	err := json.Unmarshal([]byte(req.Body), &update)
+	if err == nil && update.Message != nil {
 		reply := GetReply(update)
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 		msg.ReplyToMessageID = update.Message.MessageID
-		_, err = bot.Send(msg)
+		_, err = telegramBotApi.Send(msg)
 	}
-	return err
+	return events.APIGatewayV2HTTPResponse{StatusCode: http.StatusOK}, err
 }
 
 func main() {
